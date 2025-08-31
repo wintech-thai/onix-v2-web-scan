25aecabbe611a3826f027f1c135bb586b13f0029
pjames <pjames@gmail.com>
2025-08-31 18:36:23 +0700

    Allow 0.0.0.0

--- app.py
+++ app.py
@@ -7,8 +7,9 @@ def home():
     return "Hello, Flask!"
 
 @app.route("/verify")
+@app.route("/verify-dev")
 def verify():
     return "This is verification page."
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", debug=True)
