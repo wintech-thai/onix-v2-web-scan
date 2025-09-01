2a35675e715ab8e922c92d45971b3cf3963c7a45
pjames <pjames@gmail.com>
2025-09-01 11:08:28 +0700

    Added scan-static dir

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.Extensions.FileProviders;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -12,7 +14,14 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-app.UsePathBase("/verify-data");
+
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(
+        Path.Combine(builder.Environment.WebRootPath, "scan-static")
+    ),
+    RequestPath = "/scan-static"
+});
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
